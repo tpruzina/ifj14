4d6d1c02cafeaec60c62e444ebcada0bc0eb22ba
Tomáš Pružina <pruzinat@gmail.com>
2014-09-29 01:23:27 +0200

    Fuck this, gonna revert the shit out of it 	modified:   GC.h 	modified:   String.c

--- src/GC.h
+++ src/GC.h
@@ -10,9 +10,9 @@
 
 #include "Structs.h"
 
-void* gcMalloc(struct mainAll** ma, size_t size);
-void* gcRealloc(struct mainAll** ma, void* ptr, size_t size);
-int gcFree(struct mainAll** ma, void* ptr);
-void gcFreeAll(struct mainAll** ma);
-void gcPrintState(struct mainAll** ma);
+void* gcMalloc(struct mainAll **ma, size_t size);
+void* gcRealloc(struct mainAll **ma, void *ptr, size_t size);
+int gcFree(struct mainAll **ma, void *ptr);
+void gcFreeAll(struct mainAll **ma);
+void gcPrintState(struct mainAll **ma);
 #endif
