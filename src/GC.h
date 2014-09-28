42c3630784bd0434085f7891a44450f7d9ebf229
TondaMarko <tondamarko@gmail.com>
2014-09-28 18:40:18 +0200

    Predbezne kompletni GC

---
+++ src/GC.h
@@ -0,0 +1,18 @@
+/*
+ * GC.h
+ * 
+ * Hlavickovy soubor knihovny GC.
+ * 
+ * 
+ */
+#ifndef GC_H
+#define GC_H
+
+#include "Structs.h"
+
+void* gcMalloc(struct mainAll** ma, size_t size);
+void* gcRealloc(struct mainAll** ma, void* ptr, size_t size);
+int gcFree(struct mainAll** ma, void* ptr);
+void gcFreeAll(struct mainAll** ma);
+void gcPrintState(struct mainAll** ma);
+#endif
